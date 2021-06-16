a05eac7767cc291c33ed8f4e2c99e094b1098709
waldek <waldek@szkola-dobrego-kodu.pl>
2021-06-16 11:04:25 +0200

    add steps to meal entity

--- backend/project/db-connector/src/main/java/pl/wizard/software/diet/meals/MealEntity.java
+++ backend/project/db-connector/src/main/java/pl/wizard/software/diet/meals/MealEntity.java
@@ -2,9 +2,10 @@ package pl.wizard.software.diet.meals;
 
 import lombok.Data;
 import pl.wizard.software.AbstractBaseEntity;
+import pl.wizard.software.diet.products.ProductEntity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "MEALS")
@@ -13,5 +14,14 @@ public class MealEntity extends AbstractBaseEntity {
     private String name;
     private String author;
     private String description;
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(
+            name = "MEALS_PRODUCTS",
+            joinColumns = { @JoinColumn(name = "meal_id") },
+            inverseJoinColumns = { @JoinColumn(name = "product_id") }
+    )
+    private List<ProductEntity> products;
+    @OneToMany
+    @JoinColumn(name = "meal_id")
     private String steps;
 }
---
+++ backend/project/db-connector/src/main/java/pl/wizard/software/diet/meals/StepEntity.java
@@ -0,0 +1,14 @@
+package pl.wizard.software.diet.meals;
+
+import lombok.Data;
+import pl.wizard.software.AbstractBaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "STEPS")
+@Data
+public class StepEntity extends AbstractBaseEntity {
+    private String description;
+}
