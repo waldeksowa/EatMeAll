76226871f6d6bb660714ed025ba47b10dfb1f2a5
waldek <waldek@szkola-dobrego-kodu.pl>
2021-07-12 11:13:59 +0200

    change scope to provided for junit

--- backend/project/rest.base/src/test/java/pl/wizard/software/diet/ShopppingListServiceTest.java
+++ backend/project/rest.base/src/test/java/pl/wizard/software/diet/ShopppingListServiceTest.java
@@ -1,7 +1,14 @@
 package pl.wizard.software.diet;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopppingListServiceTest {
 
-
+    @Test
+    public void shouldReturnZero() {
+        assertEquals(0, 0);
+    }
 
 }
