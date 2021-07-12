0bdd68c4eda804344cf43942f31b7ab586e67b6f
waldek <waldek@szkola-dobrego-kodu.pl>
2021-07-12 12:04:26 +0200

    add mockito dependency to pom

--- backend/project/rest.base/src/test/java/pl/wizard/software/diet/ShopppingListServiceTest.java
+++ backend/project/rest.base/src/test/java/pl/wizard/software/diet/ShopppingListServiceTest.java
@@ -1,10 +1,18 @@
 package pl.wizard.software.diet;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import pl.wizard.software.diet.meals.MealDao;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ShopppingListServiceTest {
+
+    @Mock
+    MealDao mealRepository;
 
     @Test
     public void shouldReturnZero() {
