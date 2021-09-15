bb6d5cfbd2dd219720ab8873f1e4dbadf468ff94
waldek <waldek@szkola-dobrego-kodu.pl>
2021-09-15 16:48:04 +0200

    tests for login endpoint

--- backend/project/rest.base/src/test/java/pl/wizard/software/login/LoginAPITest.java
+++ backend/project/rest.base/src/test/java/pl/wizard/software/login/LoginAPITest.java
@@ -4,21 +4,25 @@ import io.restassured.http.ContentType;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class LoginAPITest {
 
     private static String requestBody = "{\"username\": \"admin\", \"password\": \"password\"}";
 
     @Test
-    public void shouldReturnStatusCode200() {
+    public void shouldReturnStatusCode200AndExpectedId() {
         given().contentType(ContentType.JSON).body(requestBody).
         when().post("http://localhost:8080/api/v2/login").
-        then().statusCode(200);
+        then().statusCode(200).body("accountID", equalTo(1));
     }
 
     @Test
-    public void shouldReturnValidResponse() {
-        
+    public void shouldReturnValidResponseBody() {
+        given().contentType(ContentType.JSON).body(requestBody).
+        when().post("http://localhost:8080/api/v2/login").
+        then().assertThat().body(matchesJsonSchemaInClasspath("token-schema.json"));
     }
 
 }
