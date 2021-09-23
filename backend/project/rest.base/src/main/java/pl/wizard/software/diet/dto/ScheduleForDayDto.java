829e9d224a98de71d1b67f55df388700dde3d7b5
waldek <waldek@szkola-dobrego-kodu.pl>
2021-09-23 16:47:08 +0200

    json format for scheduleForDay

--- backend/project/rest.base/src/main/java/pl/wizard/software/diet/dto/ScheduleForDayDto.java
+++ backend/project/rest.base/src/main/java/pl/wizard/software/diet/dto/ScheduleForDayDto.java
@@ -1,5 +1,6 @@
 package pl.wizard.software.diet.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class ScheduleForDayDto {
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
     private Long breakfast;
     private Long secondBreakfast;
