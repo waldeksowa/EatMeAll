b903196ea17898117dc352e4384b90867d761972
waldek <waldek@szkola-dobrego-kodu.pl>
2021-09-27 17:53:17 +0200

    revert changes in memberDao

--- backend/project/rest.base/src/main/java/pl/wizard/software/diet/MemberService.java
+++ backend/project/rest.base/src/main/java/pl/wizard/software/diet/MemberService.java
@@ -20,7 +20,7 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
-    public List<MemberEntity> findAllMembers(Long accountId) {
+    public List<MemberEntity> findAllForAccount(Long accountId) {
         return memberRepository.findAllMembers(accountId);
     }
 
