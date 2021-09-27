ec4087ff7243d9429eb4de9963f8dc3f826d8098
waldek <waldek@szkola-dobrego-kodu.pl>
2021-09-27 17:20:12 +0200

    proper sql for findByMember

--- backend/project/db-connector/src/main/java/pl/wizard/software/diet/schedules/ScheduleDao.java
+++ backend/project/db-connector/src/main/java/pl/wizard/software/diet/schedules/ScheduleDao.java
@@ -18,10 +18,10 @@ public interface ScheduleDao extends JpaRepository<ScheduleEntity, Long> {
             "where m.account_id = ?1 and s.id = ?2", nativeQuery = true)
     Optional<ScheduleEntity> findById(Long accountId, Long scheduleId);
 
-    @Query(value = "select s from ScheduleEntity s " +
-            "left join MemberEntity m on s.memberId = m.Id " +
-            "where m.accountId = ?1 and s.memberId = ?2" +
-            "order by s.date DESC" +
+    @Query(value = "select s.* from ScheduleEntity s " +
+            "left join MemberEntity m on s.member_id = m.Id " +
+            "where m.account_id = ?1 and s.member_id = ?2" +
+            "order by s.schedule_date DESC" +
             "limit 1", nativeQuery = true)
     Optional<ScheduleEntity> findByMember(Long accountId, Long memberId);
 }
