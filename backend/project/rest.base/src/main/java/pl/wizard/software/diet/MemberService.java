e96620c38800edd1d437527e31cba53152a0d0bb
waldek <waldek@szkola-dobrego-kodu.pl>
2021-09-27 17:56:28 +0200

    add transactional to member entity

--- backend/project/rest.base/src/main/java/pl/wizard/software/diet/MemberService.java
+++ backend/project/rest.base/src/main/java/pl/wizard/software/diet/MemberService.java
@@ -3,6 +3,7 @@ package pl.wizard.software.diet;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.wizard.software.diet.members.MemberDao;
 import pl.wizard.software.diet.members.MemberEntity;
 
@@ -16,18 +17,22 @@ public class MemberService {
 
     private final MemberDao memberRepository;
 
+    @Transactional
     public List<MemberEntity> findAll() {
         return memberRepository.findAll();
     }
 
+    @Transactional
     public List<MemberEntity> findAllForAccount(Long accountId) {
         return memberRepository.findAllMembers(accountId);
     }
 
+    @Transactional
     public Optional<MemberEntity> findById(Long id) {
         return memberRepository.findById(id);
     }
 
+    @Transactional
     public Optional<MemberEntity> findByIdForAccount(Long accountId, Long memberId) {
         return memberRepository.findMemberById(accountId, memberId);
     }
