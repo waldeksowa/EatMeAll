515adddb1ed44e50b476cda46e2674c1953259e4
waldek <waldek@szkola-dobrego-kodu.pl>
2021-11-19 15:58:59 +0100

    bag in autorization

--- backend/project/rest.base/src/main/java/pl/wizard/software/exception/AuthorizationFailedAdvice.java
+++ backend/project/rest.base/src/main/java/pl/wizard/software/exception/AuthorizationFailedAdvice.java
@@ -6,16 +6,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ControllerAdvice
 public class AuthorizationFailedAdvice {
 
-    @ControllerAdvice
-    public class ExerciseNotFoundAdvice {
-
-        @ResponseBody
-        @ExceptionHandler(AuthorizationFailedException.class)
-        @ResponseStatus(HttpStatus.UNAUTHORIZED)
-        public String authorizationFailedHandler(AuthorizationFailedException exception) {
-            return exception.getMessage();
-        }
+    @ResponseBody
+    @ExceptionHandler(AuthorizationFailedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String authorizationFailedHandler(AuthorizationFailedException exception) {
+        return exception.getMessage();
     }
 }
