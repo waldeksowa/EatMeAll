5a756e61322d4db6b500c24a1e1a2c6983bcb4f1
waldek <waldek@szkola-dobrego-kodu.pl>
2022-01-07 19:27:43 +0100

    change member_meals table

--- backend/docker/sql/db-init.sql
+++ backend/docker/sql/db-init.sql
@@ -359,18 +359,11 @@ ALTER TABLE public.training_plans OWNER to eatmeall;
 
 CREATE TABLE public.member_meals
 (
-    id bigint NOT NULL,
-    created_at timestamp without time zone,
-    updated_at timestamp without time zone,
-    version integer NOT NULL,
-    author character varying(255),
-    description character varying(255),
-    name character varying(255),
-    prepare_time integer,
+    meal_id bigint NOT NULL,
     member_id bigint,
-    meal_id bigint,
-    CONSTRAINT member_meals_pkey PRIMARY KEY (id),
-    CONSTRAINT fk_member_meals_meal_id FOREIGN KEY (meal_id)
+    parent_meal_id bigint,
+    CONSTRAINT member_meals_pkey PRIMARY KEY (meal_id),
+    CONSTRAINT fk_member_meals_parent_meal_id FOREIGN KEY (parent_meal_id)
             REFERENCES public.meals (id) MATCH SIMPLE
             ON UPDATE NO ACTION
             ON DELETE NO ACTION,
