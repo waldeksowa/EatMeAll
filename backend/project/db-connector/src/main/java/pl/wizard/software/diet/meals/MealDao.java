5d999a98fe14464aa5c1d325cf97c54950de0451
waldek <waldek@szkola-dobrego-kodu.pl>
2022-01-08 20:10:19 +0100

    second change

--- backend/project/db-connector/src/main/java/pl/wizard/software/diet/meals/MealDao.java
+++ backend/project/db-connector/src/main/java/pl/wizard/software/diet/meals/MealDao.java
@@ -10,7 +10,6 @@ public interface MealDao extends JpaRepository<MealEntity, Long> {
     @Query(value = "select m.*" +
                     " from meals m" +
                     " left join meal_time mt on m.id = mt.meal_entity_id" +
-                    " left join member_meals mm on m.id = mm.id" +
                     " where mt.meal_time = ?1" +
                     " order by random()" +
                     " limit ?2", nativeQuery = true)
