40cc7295a4ad6e2cc9b55dc181dc0341527350b3
waldek <waldek@szkola-dobrego-kodu.pl>
2022-01-08 19:49:30 +0100

    change query in MealDao

--- backend/project/db-connector/src/main/java/pl/wizard/software/diet/meals/MealDao.java
+++ backend/project/db-connector/src/main/java/pl/wizard/software/diet/meals/MealDao.java
@@ -2,7 +2,6 @@ package pl.wizard.software.diet.meals;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,7 +10,8 @@ public interface MealDao extends JpaRepository<MealEntity, Long> {
     @Query(value = "select m.*" +
                     " from meals m" +
                     " left join meal_time mt on m.id = mt.meal_entity_id" +
-                    " where mt.meal_time = ?1" +
+                    " left join member_meals mm on m.id = mm.id" +
+                    " where mt.meal_time = ?1 and mm.id is null" +
                     " order by random()" +
                     " limit ?2", nativeQuery = true)
     public List<MealEntity> findRandomByMealTime(int mealTime, int amount);
