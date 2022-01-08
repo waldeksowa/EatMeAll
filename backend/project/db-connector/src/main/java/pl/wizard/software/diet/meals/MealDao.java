015a279aee9c1b800ce9778af77c6ee0d07cd49d
waldek <waldek@szkola-dobrego-kodu.pl>
2022-01-08 22:00:40 +0100

    test query 5

--- backend/project/db-connector/src/main/java/pl/wizard/software/diet/meals/MealDao.java
+++ backend/project/db-connector/src/main/java/pl/wizard/software/diet/meals/MealDao.java
@@ -7,8 +7,9 @@ import java.util.List;
 
 public interface MealDao extends JpaRepository<MealEntity, Long> {
 
-    @Query(value = "select *" +
-                    " from meals m ", nativeQuery = true)
+    @Query(value = "select m.*, mm.*" +
+            " from meals m " +
+            " left join member_meals mm on m.id = mm.id ", nativeQuery = true)
     public List<MealEntity> findRandomByMealTime(int mealTime, int amount);
 
     @Query(value = "select m.* " +
