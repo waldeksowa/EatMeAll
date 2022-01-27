fe133fd64eee89144b3ca26f1ee39426c51f6e82
waldek <waldek@szkola-dobrego-kodu.pl>
2022-01-27 12:32:49 +0100

    remove attachment from sending mail

--- backend/project/rest.base/src/main/java/pl/wizard/software/email/EmailSender.java
+++ backend/project/rest.base/src/main/java/pl/wizard/software/email/EmailSender.java
@@ -6,6 +6,7 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
 class EmailSender {
@@ -20,34 +21,49 @@ class EmailSender {
     public static final String MESSAGE_TYPE = "text/html; charset=utf-8";
 
     public void send(String recipient, String messageBody, File file) {
+        Properties props = setProperties();
+        Session session = getSession(props);
+        try {
+            Message message = createMessage(recipient, messageBody, file, session);
+            Transport.send(message );
+        } catch (MessagingException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Properties setProperties() {
         Properties props = new Properties();
         props.put("mail.smtp.host", SMTP_HOST);
         props.put("mail.transport.protocol", SMTP_PROTOCOL);
         props.put("mail.smtp.port", SMTP_PORT);
         props.put("mail.smtp.auth", SMTP_AUTH);
+        return props;
+    }
 
+    private Session getSession(Properties props) {
         Authenticator authenticator = new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(SMTP_USER_NAME, SMTP_PASSWORD);
             }
         };
-
         Session session = Session.getInstance(props, authenticator);
+        return session;
+    }
 
-        try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(SMTP_USER_NAME));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
-            message.setSubject(EMAIL_SUBJECT);
-            MimeBodyPart mimeBodyPart = new MimeBodyPart();
-            mimeBodyPart.setContent(messageBody, MESSAGE_TYPE);
-            Multipart multipart = new MimeMultipart();
-            multipart.addBodyPart(mimeBodyPart);
-            message.setContent(multipart);
-            Transport.send(message );
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
+    private Message createMessage(String recipient, String messageBody, File file, Session session) throws MessagingException, IOException {
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(SMTP_USER_NAME));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
+        message.setSubject(EMAIL_SUBJECT);
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(messageBody, MESSAGE_TYPE);
+//        MimeBodyPart attachmentBodyPart = new MimeBodyPart();
+//        attachmentBodyPart.attachFile(file);
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+//        multipart.addBodyPart(attachmentBodyPart);
+        message.setContent(multipart);
+        return message;
     }
 }
---
+++ backend/project/rest.base/src/test/java/pl/wizard/software/email/EmailSenderTest.java
@@ -0,0 +1,8 @@
+package pl.wizard.software.email;
+
+import static org.junit.Assert.*;
+
+public class EmailSenderTest {
+
+
+}
