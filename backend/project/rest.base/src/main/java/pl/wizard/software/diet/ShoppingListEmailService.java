a4f51dcd7f5beb1fb6490ed9c2d53d9de084f261
waldek <waldek@szkola-dobrego-kodu.pl>
2022-02-01 09:49:34 +0100

    get concrete class from context

--- backend/project/rest.base/src/main/java/pl/wizard/software/diet/ShoppingListEmailService.java
+++ backend/project/rest.base/src/main/java/pl/wizard/software/diet/ShoppingListEmailService.java
@@ -1,6 +1,8 @@
 package pl.wizard.software.diet;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import pl.wizard.software.dto.ShoppingListDto;
 import pl.wizard.software.email.EmailSenderService;
@@ -12,14 +14,18 @@ import pl.wizard.software.email.ShoppingListAsPdfSender;
 public class ShoppingListEmailService {
 
     private final EmailSenderService emailSenderService;
+    @Autowired
+    private ApplicationContext context;
 
     public void sendEmailWithPdf(ShoppingListDto shoppingList, String recipient) {
-        ShoppingListAsPdfSender emailPdf = new ShoppingListAsPdfSender(emailSenderService);
+        ShoppingListAsPdfSender emailPdf = context.getBean(ShoppingListAsPdfSender.class, emailSenderService);
+//        ShoppingListAsPdfSender emailPdf = new ShoppingListAsPdfSender(emailSenderService);
         emailPdf.send(shoppingList, recipient);
     }
 
     public void sendEmailWithExcel(ShoppingListDto shoppingList, String recipient) {
-        ShoppingListAsExcelSender emailExcel = new ShoppingListAsExcelSender(emailSenderService);
+        ShoppingListAsExcelSender emailExcel = context.getBean(ShoppingListAsExcelSender.class, emailSenderService);
+//        ShoppingListAsExcelSender emailExcel = new ShoppingListAsExcelSender(emailSenderService);
         emailExcel.send(shoppingList, recipient);
     }
 }
--- backend/project/rest.base/src/main/java/pl/wizard/software/email/ShoppingListAsPdfSender.java
+++ backend/project/rest.base/src/main/java/pl/wizard/software/email/ShoppingListAsPdfSender.java
@@ -7,7 +7,6 @@ import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
